9b7a6a8dffcfcd5a2b6b6e36242073cbb9eb4240
mayt <mayt>
2017-09-26 10:24:33 +0800

    no message

--- src/main/java/com/example/myprocess/impl/AutoTaskImpl.java
+++ src/main/java/com/example/myprocess/impl/AutoTaskImpl.java
@@ -7,12 +7,12 @@ import org.activiti.engine.delegate.JavaDelegate;
 
 public class AutoTaskImpl implements JavaDelegate {
 
-	  public void execute(DelegateExecution execution) {
-	    String var = (String) execution.getVariable("name");
-	    var = var.toUpperCase();
-	    execution.setVariable("name", var);
-	    Map<String, Object> map = execution.getVariables();
-	    System.out.println(map);
-	  }
-
+	public void execute(DelegateExecution execution) {
+		String var = (String) execution.getVariable("name");
+		var = var.toUpperCase();
+		execution.setVariable("name", var);
+		Map<String, Object> map = execution.getVariables();
+		System.out.println(map);
 	}
+
+}
--- src/main/java/com/example/myprocess/impl/UserTaskListener.java
+++ src/main/java/com/example/myprocess/impl/UserTaskListener.java
@@ -1,29 +1,11 @@
 package com.example.myprocess.impl;
 
-import org.activiti.engine.EngineServices;
-import org.activiti.engine.RuntimeService;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
-import org.activiti.engine.delegate.event.ActivitiEvent;
-import org.activiti.engine.delegate.event.ActivitiEventListener;
 
 public class UserTaskListener implements ExecutionListener {
 
-	public void onEvent(ActivitiEvent event) {
-        switch (event.getType()) {
-        case ACTIVITY_STARTED:
-        	System.out.println("UserTaskListener.onEvent.ACTIVITY_STARTED");
-        	EngineServices engineServices = event.getEngineServices();
-        	String executionId = event.getExecutionId();
-        	RuntimeService runtimeService = engineServices.getRuntimeService();
-        	runtimeService.setVariable(executionId, "userTaskVariable", "user_task");
-        }
-	}
-
-	public boolean isFailOnException() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	private static final long serialVersionUID = -572873089377492574L;
 
 	public void notify(DelegateExecution execution) throws Exception {
 		System.out.println("UserTaskListener.notify");
